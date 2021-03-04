478c546ee1746d10c5adc3ec5d0bebd110cb91c7
Dhruv Parmar <30573895+dhruvRj18@users.noreply.github.com>
2021-03-04 16:52:55 +0000

    Update Constants.kt

--- app/src/main/java/com/example/lovecalculator/util/Constants.kt
+++ app/src/main/java/com/example/lovecalculator/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.lovecalculator.util
 class Constants {
     companion object{
         const val BASE_URL = "https://love-calculator.p.rapidapi.com/"
-        const val API_KEY = "7f0dce92dfmsh8a2b93dc287ce65p19d0b3jsn586be91ded6b"
+        const val API_KEY = "your API-Key"
         const val HEADER_HOST =  "love-calculator.p.rapidapi.com"
     }
 }
